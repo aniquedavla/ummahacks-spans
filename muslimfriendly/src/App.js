40df859c7c88c212ca5659498458a580c052eef8
Ayesha Khan <akhan1231@hotmail.com>
2020-07-23 23:14:51 -0700

    Navbar with logo made

--- muslimfriendly/src/App.js
+++ muslimfriendly/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+/* ----- Components ----- */
+import NavBar from "./components/navbar.js";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ muslimfriendly/src/components/navbar.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Moon from "../images/moon.png";
+class NavBar extends Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={Moon}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Muslim Friendly
+        </Navbar.Brand>
+      </Navbar>
+    );
+  }
+}
+
+export default NavBar;
