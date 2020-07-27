0ab887f777ee0cf84217804e26ad5f466d308fee
Ayesha Khan <akhan1231@hotmail.com>
2020-07-26 22:38:47 -0700

    amenities

--- muslimfriendly/src/App.js
+++ muslimfriendly/src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import logo from "./images/moon.png";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 /* ----- Components ----- */
 import NavBar from "./components/navbar.js";
 import HomePage from "./components/landingPage/homepage.js";
+import School from "./components/school.js";
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <HomePage />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/School" component={School} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- muslimfriendly/src/components/school.js
+++ muslimfriendly/src/components/school.js
@@ -1,18 +1,81 @@
 import React, { Component } from "react";
 import SchoolImage from "../images/school.png";
+import "./school.css";
+import { Row, Col } from "react-bootstrap";
+import star from "../images/star.png";
+import sajadah from "../images/sajadah.png";
+import hijab from "../images/hijab.png";
+import wudu from "../images/wudu.png";
+import halal from "../images/halal.png";
+import beard from "../images/beard.png";
+import cel from "../images/muslimcel.png";
+import lead from "../images/leadership.png";
+import mlead from "../images/woma.png";
 
 class School extends Component {
   render() {
     return (
       <div>
-        <div
-          classname="Banner"
-          style={{
-            backgroundImage: `url(${SchoolImagee})`,
-            height: "600px",
-            backgroundSize: "cover",
-          }}
-        ></div>
+        <img src={SchoolImage} style={{ height: "400px" }} />
+        <Row>
+          <Col>
+            <h1>San Jose State University</h1>
+            <Row style={{ paddingLeft: "30px" }}>
+              <img src={star} style={{ height: "30px" }} />
+              <h3> 4.7 out of 5 (35 Reviews)</h3>
+            </Row>
+          </Col>
+          <Col></Col>
+        </Row>
+        <br />
+        <br />
+
+        <Row>
+          <h3 style={{ paddingLeft: "30px" }}>Amenities</h3>
+          <Col>
+            <br />
+            <br />
+            <Row>
+              <img src={sajadah} style={{ height: "30px" }} />{" "}
+              <h3> Prayer Space</h3>
+            </Row>
+            <Row>
+              <img src={wudu} style={{ height: "30px" }} /> <h3> Wudu Area</h3>
+            </Row>
+            <Row>
+              <img src={halal} style={{ height: "30px" }} />{" "}
+              <h3>Halal Options</h3>
+            </Row>
+            <Row>
+              <img src={hijab} style={{ height: "30px" }} />{" "}
+              <h3>Hijab Friendly</h3>
+            </Row>
+          </Col>
+          <Col>
+            <br />
+            <br />
+            <Row>
+              <img src={beard} style={{ height: "30px" }} />
+              {"  "}
+              <h3>
+                {" "}
+                <span></span>Beard Friendly
+              </h3>
+            </Row>
+            <Row>
+              <img src={cel} style={{ height: "30px" }} />{" "}
+              <h3>Muslim Celebrations</h3>
+            </Row>
+            <Row>
+              <img src={lead} style={{ height: "30px" }} />{" "}
+              <h3>Muslim Leadership</h3>
+            </Row>
+            <Row>
+              <img src={mlead} style={{ height: "30px" }} />{" "}
+              <h3>Muslim Woman Leadership</h3>
+            </Row>
+          </Col>
+        </Row>
       </div>
     );
   }
