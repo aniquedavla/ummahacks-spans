ad1b72da78b8077e4fca0985d3933cfc29b211c2
Ayesha Khan <akhan1231@hotmail.com>
2020-07-26 18:54:35 -0700

    Landing Screen

---
+++ muslimfriendly/src/components/landingPage/homepage.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import backgroundImage from "../../images/background.png";
+import "./homepage.css";
+import ReactSearchBox from "react-search-box";
+
+class HomePage extends Component {
+  data = [
+    {
+      key: "john",
+      value: "John Doe",
+    },
+    {
+      key: "jane",
+      value: "Jane Doe",
+    },
+    {
+      key: "mary",
+      value: "Mary Phillips",
+    },
+    {
+      key: "robert",
+      value: "Robert",
+    },
+    {
+      key: "karius",
+      value: "Karius",
+    },
+  ];
+  render() {
+    return (
+      <div>
+        <div
+          classname="Banner"
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            height: "600px",
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="headingT">
+            Find out how muslim-friendly your college is
+          </div>
+          <div className="SearchBar">
+            <ReactSearchBox
+              placeholder="Search"
+              value=""
+              data={this.data}
+              callback={(record) => console.log(record)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
