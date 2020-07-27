da9bda22a87e842ca23cc270a20221485cae38e7
Ayesha Khan <akhan1231@hotmail.com>
2020-07-26 22:03:19 -0700

    Added school

--- muslimfriendly/src/components/school.js
+++ muslimfriendly/src/components/school.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
+import SchoolImage from "../images/school.png";
+
 class School extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div
+          classname="Banner"
+          style={{
+            backgroundImage: `url(${SchoolImagee})`,
+            height: "600px",
+            backgroundSize: "cover",
+          }}
+        ></div>
+      </div>
+    );
   }
 }
 
