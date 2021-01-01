77893a3c08ed1a64154f10d78b54fb723d02846c
Anique Davla <aniquedavla@gmail.com>
2020-12-31 20:15:04 -0800

    airtable submission height fix

--- src/pages/Rate/RateForm.js
+++ src/pages/Rate/RateForm.js
@@ -8,7 +8,7 @@ export default function RateForm(){
       });
     return(
         <>
-        <iframe className="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrIszaUXHVIz6U6V?backgroundColor=gray" frameborder="0" onmousewheel="" width="100%" height="2126"></iframe>
+        <iframe class="airtable-embed" src="https://airtable.com/embed/shrIszaUXHVIz6U6V?backgroundColor=gray" frameborder="0" onmousewheel="" width="100%" height="1200"></iframe>
         </>
     );
 }
